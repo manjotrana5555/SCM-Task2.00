f5f05c882dc3eb1e9c6739dbbe577a3a096bb2e4
Manpreet3033 <manpreet0855.be21@chitkara.edu.in>
2022-05-26 19:32:13 +0530

    First Commit

---
+++ BasicCalculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x , y;
+    cout << "Enter First No : ";
+    cin >> x;
+    cout << "Enter Second No : ";
+    cin >> y;
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << x / y << endl;
+    cout << x % y << endl;
+}
